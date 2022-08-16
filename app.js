5d96b36545a48d926e8574ca8bc5dea0c4711618
Tom Taylor <tomgtaylor@yahoo.com>
2022-08-16 14:08:52 -0700

    Started refactoring JS code.

--- app.js
+++ app.js
@@ -2,190 +2,214 @@
 
 //------------------------------ Array to hold all objects
 
-let allProducts = []
+let imageArray = [];
 
-//------------------------------ Constructor function and declared variables
+//------------------------------ Variable to count rounds
 
-function Product(name, imgUrl) {
+let rounds = 0;
+
+//------------------------------ Constructor function images. Has two parameters.
+
+function Image(name, id) {
     this.name = name;
-    this.imgUrl = imgUrl;
+    this.id = id;       
+    this.src = `img/${id}`;
     this.clicks = 0;
     this.views = 0;
-    
-    allProducts.push(this);
+
+    imageArray.push(this);  // Pushes each Image object into array on line 5.
 }
 
-let bag = new Product('bag', 'bag.jpg');
-let banana = new Product('banana', 'banana.jpg');
-let bathroom = new Product('bathroom', 'bathroom.jpg');
-let boots = new Product('boots', 'boots.jpg');
-let breakfast = new Product('breakfast', 'breakfast.jpg');
-let bubblegum = new Product('bubblegum', 'bubblegum.jpg');
-let chair = new Product('chair', 'chair.jpg');
-let cthulhu = new Product('cthulhu', 'cthulhu.jpg');
-let dogDuck = new Product('dog-duck', 'dog-duck.jpg');
-let dragon = new Product('dragon', 'dragon.jpg');
-let pen = new Product('pen', 'pen.jpg');
-let petSweep = new Product('pet-sweep', 'pet-sweep.jpg');
-let scissors = new Product('scissors', 'scissors.jpg');
-let shark = new Product('shark', 'shark.jpg');
-let sweep = new Product('sweep', 'sweep.png');  // only .png image
-let tauntaun = new Product('tauntaun', 'tauntaun.jpg');
-let unicorn = new Product('unicorn', 'unicorn.jpg');
-let waterCan = new Product('water-can', 'water-can.jpg');
-let wineGlass = new Product('wine-glass', 'wine-glass.jpg');
+//------------------------------ Array for new products. Each includes two arguments.
 
-// console.log(allProducts[15]);
+let newProducts = [
 
-//------------------------------ Variable: Counts total rounds. ++ each time an image is clicked
-
-let currentRound = 0;
-
-//------------------------------- Function: Generates random Image
+    new Product('bag', 'bag.jpg'),
+    new Product('banana', 'banana.jpg'),
+    new Product('bathroom', 'bathroom.jpg'),
+    new Product('boots', 'boots.jpg'),
+    new Product('breakfast', 'breakfast.jpg'),
+    new Product('bubblegum', 'bubblegum.jpg'),
+    new Product('chair', 'chair.jpg'),
+    new Product('cthulhu', 'cthulhu.jpg'),
+    new Product('dog-duck', 'dog-duck.jpg'),
+    new Product('dragon', 'dragon.jpg'),
+    new Product('pen', 'pen.jpg'),
+    new Product('pet-sweep', 'pet-sweep.jpg'),
+    new Product('scissors', 'scissors.jpg'),
+    new Product('shark', 'shark.jpg'),
+    new Product('sweep', 'sweep.png'),  // only .png image
+    new Product('tauntaun', 'tauntaun.jpg'),
+    new Product('unicorn', 'unicorn.jpg'),
+    new Product('water-can', 'water-can.jpg'),
+    new Product('wine-glass', 'wine-glass.jpg'),
+];
+    
+//------------------------------ Function: generates a random index/image in array on line 5.
 
 function randomImage() {
-    return Math.floor(Math.random() * allProducts.length);
+    let index = Math.floor(Math.random() * imageArray.length); 
+    return imageArray[i];
 }
 
-//------------------------------ Function upon page load...show new image..
-function onPageLoad() {
-    let product1 = allProducts[randomImage()]; 
-    let product2 = allProducts[randomImage()];  
-    let product3 = allProducts[randomImage()];
+//------------------------------ Function to display images
+
+function displayImages() {
+    let image1 = randomImage();
+    let image2 = randomImage();
     
-    let image1 = document.getElementById('product-image1'); 
-    image1.src = `img/${product1.imgUrl}`;
-    
-    let image2 = document.getElementById('product-image2');
-    image2.src = `img/${product2.imgUrl}`;
-    
-    let image3 = document.getElementById('product-image3');
-    image3.src = `img/${product3.imgUrl}`;
-}
-onPageLoad();
-
-//------------------------------- Event Listener for each button
-
-let button1 = document.getElementById('product-button1');
-button1.addEventListener('click', showNewImage);
-
-let button2 = document.getElementById('product-button2');
-button2.addEventListener('click', showNewImage);
-
-let button3 = document.getElementById('product-button3');
-button3.addEventListener('click', showNewImage);
-
-
-
-//------------------------------- Function: display new 3 images 
-
-function showNewImage(event) {  //event is being clicked
-    // console.log(event);
-    
-    // Selects random object (image) from array.
-    let product1 = allProducts[randomImage()]; 
-    let product2 = allProducts[randomImage()];  
-    let product3 = allProducts[randomImage()];
-
-    // let array = ;
-
-    // while(product1 === product2 || product1 === product3 || product2 === product3){
-
-    // }
-
-    product1.views++;   // Increments views per object randomly selected.
-    product2.views++;
-    product3.views++;
-    
-    let image1 = document.getElementById('product-image1'); 
-    image1.src = `img/${product1.imgUrl}`;
-    image1.alt = product1.name;
-    
-    let image2 = document.getElementById('product-image2');
-    image2.src = `img/${product2.imgUrl}`;
-    image2.alt = product2.name;
-    
-    let image3 = document.getElementById('product-image3');
-    image3.src = `img/${product3.imgUrl}`;
-    image3.alt = product3.name;
-
-    console.log(event);
-
-    currentRound++;
-
-        //-----------------------------------For loop: to get clicked total per object
-
-    let altName = event.target.alt;
-    for (let i = 0; i < allProducts.length; i++){
-        if (allProducts[i].name == altName) {
-
-            console.log(allProducts[i].name);
-
-            allProducts[i].clicks++;
-        }
-    }
-
-        if (currentRound === 5) {
-        button1.removeEventListener('click', showNewImage);
-        button2.removeEventListener('click', showNewImage);
-        button3.removeEventListener('click', showNewImage);
-    }
 }
 
-// showNewImage();
+//------------------------------ 
 
-//------------------------------- Function:
+//------------------------------ 
 
-//------------------------------- 
+//------------------------------ 
 
-console.log(allProducts);
-
-
+//------------------------------ 
 
 
 
 
 
-// //------------------------------- Function showNewImage
 
-// function showNewImage(event) {
-//     let altName = event.target.alt;
-//     for (let i=0; i<allProducts.length; i++){
-//         if (allProducts[i].name == altName) {
-//             allProducts[i].clicked++;
-//         }
-//     }
-//     console.log(event.target);
-//     let product1 = allProducts[randomImage()];     // Gets random product from array.
-//     let product2 = allProducts[randomImage()];     
-//     let product3 = allProducts[randomImage()];     
-//     product1.views++;   //increments object views.
+// let allProducts = []
+
+// //------------------------------ Constructor function and declared variables
+
+// function Product(name, imgUrl) {
+//     this.name = name;
+//     this.imgUrl = imgUrl;
+//     this.clicks = 0;
+//     this.views = 0;
+    
+//     allProducts.push(this);
+// }
+
+// let bag = new Product('bag', 'bag.jpg');
+// let banana = new Product('banana', 'banana.jpg');
+// let bathroom = new Product('bathroom', 'bathroom.jpg');
+// let boots = new Product('boots', 'boots.jpg');
+// let breakfast = new Product('breakfast', 'breakfast.jpg');
+// let bubblegum = new Product('bubblegum', 'bubblegum.jpg');
+// let chair = new Product('chair', 'chair.jpg');
+// let cthulhu = new Product('cthulhu', 'cthulhu.jpg');
+// let dogDuck = new Product('dog-duck', 'dog-duck.jpg');
+// let dragon = new Product('dragon', 'dragon.jpg');
+// let pen = new Product('pen', 'pen.jpg');
+// let petSweep = new Product('pet-sweep', 'pet-sweep.jpg');
+// let scissors = new Product('scissors', 'scissors.jpg');
+// let shark = new Product('shark', 'shark.jpg');
+// let sweep = new Product('sweep', 'sweep.png');  // only .png image
+// let tauntaun = new Product('tauntaun', 'tauntaun.jpg');
+// let unicorn = new Product('unicorn', 'unicorn.jpg');
+// let waterCan = new Product('water-can', 'water-can.jpg');
+// let wineGlass = new Product('wine-glass', 'wine-glass.jpg');
+
+// // console.log(allProducts[15]);
+
+// //------------------------------ Variable: Counts total rounds. ++ each time an image is clicked
+
+// let currentRound = 0;
+
+// //------------------------------- Function: Generates random Image
+
+// function randomImage() {
+//     return Math.floor(Math.random() * allProducts.length);
+// }
+
+// //------------------------------ Function upon page load...show new image..
+// function onPageLoad() {
+//     let product1 = allProducts[randomImage()]; 
+//     let product2 = allProducts[randomImage()];  
+//     let product3 = allProducts[randomImage()];
+    
+//     let image1 = document.getElementById('product-image1'); 
+//     image1.src = `img/${product1.imgUrl}`;
+    
+//     let image2 = document.getElementById('product-image2');
+//     image2.src = `img/${product2.imgUrl}`;
+    
+//     let image3 = document.getElementById('product-image3');
+//     image3.src = `img/${product3.imgUrl}`;
+// }
+// onPageLoad();
+
+// //------------------------------- Event Listener for each button
+
+// let button1 = document.getElementById('product-button1');
+// button1.addEventListener('click', showNewImage);
+
+// let button2 = document.getElementById('product-button2');
+// button2.addEventListener('click', showNewImage);
+
+// let button3 = document.getElementById('product-button3');
+// button3.addEventListener('click', showNewImage);
+
+
+
+// //------------------------------- Function: display new 3 images 
+
+// function showNewImage(event) {  //event is being clicked
+//     // console.log(event);
+    
+//     // Selects random object (image) from array.
+//     let product1 = allProducts[randomImage()]; 
+//     let product2 = allProducts[randomImage()];  
+//     let product3 = allProducts[randomImage()];
+
+//     // let array = ;
+
+//     // while(product1 === product2 || product1 === product3 || product2 === product3){
+
+//     // }
+
+//     product1.views++;   // Increments views per object randomly selected.
 //     product2.views++;
 //     product3.views++;
     
-//     let image1 = document.getElementById('product-image1');     // Selects the image.
-//     image1.src = `img/${product1.imgUrl}`;      // Makes image the product.
+//     let image1 = document.getElementById('product-image1'); 
+//     image1.src = `img/${product1.imgUrl}`;
 //     image1.alt = product1.name;
-
-//     let image2 = document.getElementById('product-image2');     // Selects the image.
-//     image2.src = `img/${product2.imgUrl}`;      // Makes image the product.
-//     image2.alt = product2.name;
-
-//     let image3 = document.getElementById('product-image3');     // Selects the image.
-//     image3.src = `img/${product3.imgUrl}`;      // Makes image the product.
-//     image3.alt = product3.name;    
     
+//     let image2 = document.getElementById('product-image2');
+//     image2.src = `img/${product2.imgUrl}`;
+//     image2.alt = product2.name;
+    
+//     let image3 = document.getElementById('product-image3');
+//     image3.src = `img/${product3.imgUrl}`;
+//     image3.alt = product3.name;
+
+//     console.log(event);
+
 //     currentRound++;
-        
-//         if (currentRound === 5) {
-//         button.removeEventListener('click', showNewImage);
-        
+
+//         //-----------------------------------For loop: to get clicked total per object
+
+//     let altName = event.target.alt;
+//     for (let i = 0; i < allProducts.length; i++){
+//         if (allProducts[i].name == altName) {
+
+//             console.log(allProducts[i].name);
+
+//             allProducts[i].clicks++;
+//         }
 //     }
-//         console.log(allProducts);
+
+//         if (currentRound === 5) {
+//         button1.removeEventListener('click', showNewImage);
+//         button2.removeEventListener('click', showNewImage);
+//         button3.removeEventListener('click', showNewImage);
+//     }
 // }
 
-// // //------------------------------- Call function showNewImage
 // // showNewImage();
+
+// //------------------------------- Function:
+
+// //------------------------------- 
+
+// console.log(allProducts);
 
 
 
